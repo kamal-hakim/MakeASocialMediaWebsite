f51eb79c8585065c75566a7eef61419924eeb3ab
K-PC\K <kamalhakim.zainal@gmail.com>
2018-01-17 22:17:26 +0800

    Storing values in session variables

--- register.php
+++ register.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 $con = mysqli_connect("localhost", "root", "", "social"); //conection variable
 
 if(mysqli_connect_errno())
@@ -24,21 +25,24 @@ if(isset($_POST['register_button']))
     $fname = strip_tags($_POST['reg_fname']); //strip_tags is for security purpose. It strips out HTML tags in the form
     $fname = str_replace(' ', '', $fname); // remove spaces
     $fname = ucfirst(strtolower($fname)); // uppercase first letter
-
+    $_SESSION['reg_fname'] = $fname; //stores first name into session variable
     //Last name
     $lname = strip_tags($_POST['reg_lname']); //strip_tags is for security purpose. It strips out HTML tags in the form
     $lname = str_replace(' ', '', $lname); // remove spaces
     $lname = ucfirst(strtolower($lname)); // uppercase first letter
+    $_SESSION['reg_lname'] = $lname; //stores last name into session variable
 
     //email
     $em = strip_tags($_POST['reg_email']); //strip_tags is for security purpose. It strips out HTML tags in the form
     $em = str_replace(' ', '', $em); // remove spaces
     $em = ucfirst(strtolower($em)); // uppercase first letter
+    $_SESSION['reg_email'] = $em; //stores email into session variable
 
     //email confirmation
     $em2 = strip_tags($_POST['reg_email2']); //strip_tags is for security purpose. It strips out HTML tags in the form
     $em2 = str_replace(' ', '', $em2); // remove spaces
     $em2 = ucfirst(strtolower($em2)); // uppercase first letter
+    $_SESSION['reg_email2'] = $em2; //stores email into session variable
 
     //password
     $password = strip_tags($_POST['reg_password']); //strip_tags is for security purpose. It strips out HTML tags in the form
@@ -104,13 +108,30 @@ if(isset($_POST['register_button']))
   </head>
   <body>
     <form class="" action="register.php" method="post">
-      <input type="text" name="reg_fname" placeholder="First Name" required>
+      <input type="text" name="reg_fname" placeholder="First Name" value ="<?php
+      if(isset($_SESSION['reg_fname'])) {
+          echo $_SESSION['reg_fname'];
+      }
+      ?>" required>
+
       <br>
-      <input type="text" name="reg_lname" placeholder="Last Name" required>
+      <input type="text" name="reg_lname" placeholder="Last Name" value ="<?php
+      if(isset($_SESSION['reg_lname'])) {
+          echo $_SESSION['reg_lname'];
+      }
+      ?>"required>
       <br>
-      <input type="email" name="reg_email" placeholder="Email" required>
+      <input type="email" name="reg_email" placeholder="Email" value ="<?php
+      if(isset($_SESSION['reg_email'])) {
+          echo $_SESSION['reg_email'];
+      }
+      ?>" required>
       <br>
-      <input type="email" name="reg_email2" placeholder="Confirm Email" required>
+      <input type="email" name="reg_email2" placeholder="Confirm Email" value ="<?php
+      if(isset($_SESSION['reg_email2'])) {
+          echo $_SESSION['reg_email2'];
+      }
+      ?>" required>
       <br>
       <input type="password" name="reg_password" placeholder="Password" required>
       <br>
