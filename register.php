adcfafd5b995697440af4fac43da1e9d6fc79e5c
K-PC\K <kamalhakim.zainal@gmail.com>
2018-01-17 21:14:17 +0800

    Create registration form

---
+++ register.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Welcome to Swirlfeed</title>
+  </head>
+  <body>
+    <form class="" action="register.php" method="post">
+      <input type="text" name="reg_fname" placeholder="First Name" required>
+      <br>
+      <input type="text" name="reg_lname" placeholder="Last Name" required>
+      <br>
+      <input type="text" name="reg_email" placeholder="Email" required>
+      <br>
+      <input type="text" name="reg_email2" placeholder="Confirm Email" required>
+      <br>
+      <input type="text" name="reg_password" placeholder="Password" required>
+      <br>
+      <input type="text" name="reg_password2" placeholder="Confirm Password" required>
+      <br>
+      <input type="submit" name="register" value="Register">
+    </form>
+  </body>
+</html>
